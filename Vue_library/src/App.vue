46ce828b15069399178f61d14cff959b59c5433e
juntae <rlawnsxo131@gmail.com>
2018-09-23 18:05:34 +0900

    vue + library practice

---
+++ Vue_library/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <button type="button" name="button" class="btn btn-primary btn-lg">Large button</button>
+    <Chart></Chart>
+  </div>
+</template>
+
+<script>
+import Chart from './Chart.vue'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+
+    }
+  },
+  components: {
+      'Chart': Chart
+  }
+}
+</script>
+
+<style>
+
+</style>
